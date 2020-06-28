30dea8b1b39bddaadfac1566b95e4c147f2f3d62
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-06-27 19:49:39 -0500

    need to refactor how cities are stored in localStorage && beginning to capture 5 day forecast data

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,12 @@ var cityInputField = document.getElementById('city-input');
 var cityDisplayed = document.getElementById('current-city-displayed');
 var searchField = document.getElementById('search-form');
 var citiesArray = [];
+var currentCityTemp = document.getElementById('current-city-temp');
+var currentCityHumidity = document.getElementById('current-city-humidity');
+var currentCityWind = document.getElementById('current-city-wind');
+var currentCityUV = document.getElementById('current-city-uv');
+var currentCityIcon = document.getElementById('weather-icon');
+
 
 
 
@@ -27,16 +33,62 @@ var formSubmitHandler = function (event) {
 
   console.log(currentCity);
 
+
   fetch('https://api.openweathermap.org/data/2.5/weather?q=' +
     currentCity +
     '&appid=64691d0a710691e67381e1108d1f040d'
   )
 
-    .then(function (response) {
-      return response.json();
+    .then(function (weatherResponse) {
+      return weatherResponse.json();
     })
-    .then(function (response) {
-      console.log(response);
+    .then(function (weatherResponse) {
+      // console.log("temp: " + weatherResponse.main.feels_like, "humidity: " + weatherResponse.main.humidity, "wind speed: " + weatherResponse.wind.speed, "longitude: " + weatherResponse.coord.lon, "latitude: " + weatherResponse.coord.lat, "icon" + weatherResponse.weather.icon);
+
+      fetch(
+        'http://api.openweathermap.org/data/2.5/uvi?appid=64691d0a710691e67381e1108d1f040d&lat=' +
+        weatherResponse.coord.lat +
+        '&lon=' +
+        weatherResponse.coord.lon
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (response) {
+          var uvIndex = response.value
+
+          var temperatureValue = JSON.stringify(weatherResponse.main.feels_like);
+          var humidityValue = JSON.stringify(weatherResponse.main.humidity);
+          var windSpeedValue = JSON.stringify(weatherResponse.wind.speed)
+          // var weatherIcon = JSON.stringify(weatherResponse.weather.icon);
+          var uvIndexValue = JSON.stringify(uvIndex);
+
+          // Can I not reconstruct the URL somehow? If I could just get that stupid value...
+          // var iconUrl = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+
+
+          // Append current weather values to <span> elements within the "current city" card.
+          currentCityTemp.textContent = temperatureValue;
+          currentCityHumidity.textContent = humidityValue;
+          currentCityWind.textContent = windSpeedValue;
+          currentCityUV.textContent = uvIndexValue;
+          // currentCityIcon.setAttribute("src", iconUrl);
+        })
+    })
+    fiveDayForecast(currentCity);
+};
+
+
+function fiveDayForecast(currentCity) {
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q=' +
+    currentCity +
+    '&appid=64691d0a710691e67381e1108d1f040d'
+  )
+    .then(function (forecastResponse) {
+      return forecastResponse.json();
+    })
+    .then(function (forecastResponse) {
+      console.log(forecastResponse);
     })
 };
 
@@ -45,6 +97,5 @@ var formSubmitHandler = function (event) {
 
 
 
-
-
+// renderPastCities();
 searchField.addEventListener("submit", formSubmitHandler);
